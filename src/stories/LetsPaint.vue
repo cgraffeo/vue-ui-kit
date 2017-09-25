79bfe15655e9ed47e12ec5b893fd1313acdb8bf3
Chris Graffeo <cgraffeo1@gmail.com>
2017-09-25 12:24:48 -0700

    Completed lets paint functionality

--- src/stories/LetsPaint.vue
+++ src/stories/LetsPaint.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="container" :class="{'painted': background.paint}" :style="styleObject">
-    <input type="color" placeholder="#FFFFFF"></input>
+  <div class="container" :style="bgColor">
+    <div class="centered">
+      <h1>Paint this background!</h1>
+      <input type="color" v-model="bgColor.backgroundColor"></input>
+    </div>
   </div>
 </template>
 <script>
   export default {
     data() {
       return{
-        styleObject: {
-          background: '#FFFFFF'
+        bgColor: {
+          backgroundColor: "#FFFFFF"
         }
       }
     }
   }
 </script>
-<style></style>
+<style>
+  body, html, #root {
+    height: 98vw;
+    width: 98vw;
+  }
+  .centered {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-top: -50px;
+    margin-left: -100px;
+  }
+  .container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
